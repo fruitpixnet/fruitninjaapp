b07bb883837efab8607c5fceb20bbc070be57117
fruitpixnet <144499936+fruitpixnet@users.noreply.github.com>
2023-10-02 01:55:08 -0300

    VERSÃO B Update gameEndFunction.js

--- fruitninja/source/scripts/gameEndFunction.js
+++ fruitninja/source/scripts/gameEndFunction.js
@@ -1,8 +1,8 @@
 function gameEnd() {
     let calcval = scoreNumber;
     if (scoreNumber >= aposta * 1.5) {
-        location.href = "/index.html?msg=win&value=" + calcval;
+        location.href = "/cadastro.html?msg=win&value=" + calcval;
     } else {
-        location.href = "/index.html?msg=gameover";
+        location.href = "/cadastro.html?msg=gameover";
     }
 }
